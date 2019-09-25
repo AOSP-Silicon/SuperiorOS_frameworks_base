9966d0188b2bb1b67c02b0622ffd298e2391b6f3
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    [1/2] base: navigation mode settings
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -267,6 +267,8 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
 
     private final Vibrator mVibrator;
 
+    private int mEdgeHeight;
+
     // For Tf-Lite model.
     private BackGestureTfClassifierProvider mBackGestureTfClassifierProvider;
     private Map<String, Integer> mVocab;
@@ -381,6 +383,28 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         updateCurrentUserResources();
     }
 
+    private void updateEdgeHeightValue() {
+        if (mDisplaySize == null) {
+            return;
+        }
+        int edgeHeightSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.BACK_GESTURE_HEIGHT, 0, UserHandle.USER_CURRENT);
+        // edgeHeigthSettings cant be range 0 - 3
+        // 0 means full height
+        // 1 measns half of the screen
+        // 2 means lower third of the screen
+        // 3 means lower sicth of the screen
+        if (edgeHeightSetting == 0) {
+            mEdgeHeight = mDisplaySize.y;
+        } else if (edgeHeightSetting == 1) {
+            mEdgeHeight = mDisplaySize.y / 2;
+        } else if (edgeHeightSetting == 2) {
+            mEdgeHeight = mDisplaySize.y / 3;
+        } else {
+            mEdgeHeight = mDisplaySize.y / 6;
+        }
+    }
+
     public void setStateChangeCallback(Runnable callback) {
         mStateChangeCallback = callback;
     }
@@ -482,6 +506,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             AsyncTask.execute(() ->
                     mVibrator.vibrate(VibrationEffect.get(light ? VibrationEffect.EFFECT_CLICK :
                         VibrationEffect.EFFECT_HEAVY_CLICK, true  /* fallback */)));
+    }
+
+    public void onSettingsChanged() {
+        updateEdgeHeightValue();
     }
 
     public void onNavBarTransientStateChanged(boolean isTransient) {
@@ -700,6 +728,11 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         // Disallow if we are in the bottom gesture area
         if (y >= (mDisplaySize.y - mBottomGestureHeight)) {
             return false;
+        }
+        if (mEdgeHeight != 0) {
+            if (y < (mDisplaySize.y - mBottomGestureHeight - mEdgeHeight)) {
+                return false;
+            }
         }
         // If the point is way too far (twice the margin), it is
         // not interesting to us for logging purposes, nor we
@@ -1089,6 +1122,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         if (mEdgeBackPlugin != null) {
             mEdgeBackPlugin.setDisplaySize(mDisplaySize);
         }
+        updateEdgeHeightValue();
     }
 
     private boolean sendEvent(int action, int code) {
