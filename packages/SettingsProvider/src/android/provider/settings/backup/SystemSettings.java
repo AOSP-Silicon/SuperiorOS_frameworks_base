f47f44e557058d6af18d1a7fb3330074240ed43d
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    [1/2] base: navigation mode settings
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,5 +93,6 @@ public class SystemSettings {
         Settings.System.LOCKSCREEN_BATTERY_INFO,
         Settings.System.ENABLE_RIPPLE_EFFECT,
 	Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+        Settings.System.BACK_GESTURE_HEIGHT,
     };
 }
