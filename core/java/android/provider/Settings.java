4b17f5c5508236a33ef889917f75504f2b678177
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    base: QS Transparency level [1/2]
    Thanks to @Spark-Rom
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5858,6 +5858,11 @@ public final class Settings {
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * These are all public system settings
          *
          * @hide
