PixelExperience/frameworks_base

SystemUI: Update statusbar burn-in protection shift base padding when orientation changed
Statusbar base padding is calculated on FinishInflate only. This causes issues
on devices with different statusbar padding for portrait and landscape. So fix
it by update base padding on ConfigurationChanged as well.

Change-Id: I64affc655cd5c6c879a5688c7dc05be9fbbefd6e

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -200,6 +200,13 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+
+        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
+                                                                                       mBasePaddingLeft = mStatusBarContents.getPaddingStart();
+        mBasePaddingTop = mStatusBarContents.getPaddingTop();
+        mBasePaddingRight = mStatusBarContents.getPaddingEnd();
+        mBasePaddingBottom = mStatusBarContents.getPaddingBottom();
+
         updateResources();
 
         // May trigger cutout space layout-ing

AOSP-Silicon/SuperiorOS_frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: str143 <alexfinhart@gmail.com>
Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -150,6 +150,14 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks, TunerS
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+
+        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
+
+        mLeftPad = mStatusBarContents.getPaddingStart();
+        mRightPad = mStatusBarContents.getPaddingEnd();
+        mBasePaddingTop = mStatusBarContents.getPaddingTop();
+        mBasePaddingBottom = mStatusBarContents.getPaddingBottom();
+    
         updateResources();
 
         // May trigger cutout space layout-ing