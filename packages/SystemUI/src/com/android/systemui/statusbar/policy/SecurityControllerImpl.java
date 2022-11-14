LineageOS/android_frameworks_base

SystemUI: Add VPN tile
Allows quickly connecting/disconnecting to/from VPNs.

Change-Id: Iaa9413bf1071805be8764d92bc5e2dd3131fdaed

packages/SystemUI/src/com/android/systemui/statusbar/policy/SecurityControllerImpl.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.policy;
 import static com.android.systemui.Dependency.BG_HANDLER_NAME;
 
 import android.app.ActivityManager;
+import android.app.AppOpsManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -49,12 +50,15 @@ import android.util.SparseArray;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.net.LegacyVpnInfo;
 import com.android.internal.net.VpnConfig;
+import com.android.internal.net.VpnProfile;
 import com.android.systemui.R;
 import com.android.systemui.settings.CurrentUserTracker;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -81,6 +85,7 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
     private static final int CA_CERT_LOADING_RETRY_TIME_IN_MS = 30_000;
 
     private final Context mContext;
+    private final AppOpsManager mAppOpsManager;
     private final ConnectivityManager mConnectivityManager;
     private final IConnectivityManager mConnectivityManagerService;
     private final DevicePolicyManager mDevicePolicyManager;
@@ -111,6 +116,8 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
         super(context);
         mContext = context;
         mBgHandler = bgHandler;
+        mAppOpsManager = (AppOpsManager)
+                context.getSystemService(Context.APP_OPS_SERVICE);
         mDevicePolicyManager = (DevicePolicyManager)
                 context.getSystemService(Context.DEVICE_POLICY_SERVICE);
         mConnectivityManager = (ConnectivityManager)
@@ -193,6 +200,82 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
             return getNameForVpnConfig(cfg, new UserHandle(mVpnUserId));
         } else {
             return null;
+        }
+    }
+
+    @Override
+    public List<VpnProfile> getConfiguredLegacyVpns() {
+        try {
+            return Arrays.asList(mConnectivityManagerService.getAllLegacyVpns());
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to connect", e);
+            return new ArrayList<VpnProfile>();
+        }
+    }
+
+    @Override
+    public List<String> getVpnAppPackageNames() {
+        List<String> result = new ArrayList<>();
+        List<AppOpsManager.PackageOps> apps = mAppOpsManager.getPackagesForOps(
+                new int[] {AppOpsManager.OP_ACTIVATE_VPN});
+        if (apps != null) {
+            for (AppOpsManager.PackageOps pkg : apps) {
+                if (mVpnUserId != UserHandle.getUserId(pkg.getUid())) {
+                    continue;
+                }
+                // Look for a MODE_ALLOWED permission to activate VPN.
+                boolean allowed = false;
+                for (AppOpsManager.OpEntry op : pkg.getOps()) {
+                    if (op.getOp() == AppOpsManager.OP_ACTIVATE_VPN &&
+                            op.getMode() == AppOpsManager.MODE_ALLOWED) {
+                        allowed = true;
+                        break;
+                    }
+                }
+                if (allowed) {
+                    result.add(pkg.getPackageName());
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public void connectLegacyVpn(VpnProfile profile) {
+        try {
+            mConnectivityManagerService.startLegacyVpn(profile);
+        } catch (IllegalStateException | RemoteException e) {
+            Log.e(TAG, "Failed to connect", e);
+        }
+    }
+
+    @Override
+    public void launchVpnApp(String packageName) {
+        try {
+            UserHandle user = UserHandle.of(mCurrentUserId);
+            Context userContext = mContext.createPackageContextAsUser(
+                    mContext.getPackageName(), 0 /* flags */, user);
+            PackageManager pm = userContext.getPackageManager();
+            Intent appIntent = pm.getLaunchIntentForPackage(packageName);
+            if (appIntent != null) {
+                userContext.startActivityAsUser(appIntent, user);
+            }
+        } catch (NameNotFoundException nnfe) {
+            Log.w(TAG, "VPN provider does not exist: " + packageName, nnfe);
+        }
+    }
+
+    @Override
+    public void disconnectPrimaryVpn() {
+        VpnConfig cfg = mCurrentVpns.get(mVpnUserId);
+        if (cfg != null) {
+            final String user = cfg.legacy ? VpnConfig.LEGACY_VPN : cfg.user;
+            try {
+                mConnectivityManagerService.prepareVpn(user, VpnConfig.LEGACY_VPN, mVpnUserId);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Failed to disconnect", e);
+            }
         }
     }
 
@@ -312,7 +395,8 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
 
     private String getNameForVpnConfig(VpnConfig cfg, UserHandle user) {
         if (cfg.legacy) {
-            return mContext.getString(R.string.legacy_vpn_name);
+            return cfg.session != null
+                    ? cfg.session : mContext.getString(R.string.legacy_vpn_name);
         }
         // The package name for an active VPN is stored in the 'user' field of its VpnConfig
         final String vpnPackage = cfg.user;

AOSP-Silicon/SuperiorOS_frameworks_base

Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/SecurityControllerImpl.java
@@ -17,6 +17,7 @@ package com.android.systemui.statusbar.policy;
 
 import android.annotation.Nullable;
 import android.app.ActivityManager;
+import android.app.AppOpsManager;
 import android.app.admin.DeviceAdminInfo;
 import android.app.admin.DevicePolicyManager;
 import android.app.admin.DevicePolicyManager.DeviceOwnerType;
@@ -51,6 +52,7 @@ import androidx.annotation.NonNull;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.net.LegacyVpnInfo;
 import com.android.internal.net.VpnConfig;
+import com.android.internal.net.VpnProfile;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.SysUISingleton;
@@ -64,6 +66,8 @@ import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Executor;
 
 import javax.inject.Inject;
@@ -85,6 +89,7 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
     private static final int CA_CERT_LOADING_RETRY_TIME_IN_MS = 30_000;
 
     private final Context mContext;
+    private final AppOpsManager mAppOpsManager;
     private final ConnectivityManager mConnectivityManager;
     private final VpnManager mVpnManager;
     private final DevicePolicyManager mDevicePolicyManager;
@@ -115,6 +120,8 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
     ) {
         super(broadcastDispatcher);
         mContext = context;
+        mAppOpsManager = (AppOpsManager)
+                context.getSystemService(Context.APP_OPS_SERVICE);
         mDevicePolicyManager = (DevicePolicyManager)
                 context.getSystemService(Context.DEVICE_POLICY_SERVICE);
         mConnectivityManager = (ConnectivityManager)
@@ -197,6 +204,73 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
             return getNameForVpnConfig(cfg, new UserHandle(mVpnUserId));
         } else {
             return null;
+        }
+    }
+
+    @Override
+    public List<VpnProfile> getConfiguredLegacyVpns() {
+        return Arrays.asList(mVpnManager.getAllLegacyVpns());
+    }
+
+    @Override
+    public List<String> getVpnAppPackageNames() {
+        List<String> result = new ArrayList<>();
+        List<AppOpsManager.PackageOps> apps = mAppOpsManager.getPackagesForOps(
+                new int[] {AppOpsManager.OP_ACTIVATE_VPN});
+        if (apps != null) {
+            for (AppOpsManager.PackageOps pkg : apps) {
+                if (mVpnUserId != UserHandle.getUserId(pkg.getUid())) {
+                    continue;
+                }
+                // Look for a MODE_ALLOWED permission to activate VPN.
+                boolean allowed = false;
+                for (AppOpsManager.OpEntry op : pkg.getOps()) {
+                    if (op.getOp() == AppOpsManager.OP_ACTIVATE_VPN
+                            && op.getMode() == AppOpsManager.MODE_ALLOWED) {
+                        allowed = true;
+                        break;
+                    }
+                }
+                if (allowed) {
+                    result.add(pkg.getPackageName());
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public void connectLegacyVpn(VpnProfile profile) {
+        try {
+            mVpnManager.startLegacyVpn(profile);
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "Failed to connect", e);
+        }
+    }
+
+    @Override
+    public void launchVpnApp(String packageName) {
+        try {
+            UserHandle user = UserHandle.of(mCurrentUserId);
+            Context userContext = mContext.createPackageContextAsUser(
+                    mContext.getPackageName(), 0 /* flags */, user);
+            PackageManager pm = userContext.getPackageManager();
+            Intent appIntent = pm.getLaunchIntentForPackage(packageName);
+            if (appIntent != null) {
+                userContext.startActivityAsUser(appIntent, user);
+            }
+        } catch (NameNotFoundException nnfe) {
+            Log.w(TAG, "VPN provider does not exist: " + packageName, nnfe);
+        }
+    }
+
+    @Override
+    public void disconnectPrimaryVpn() {
+        VpnConfig cfg = mCurrentVpns.get(mVpnUserId);
+        if (cfg != null) {
+            final String user = cfg.legacy ? VpnConfig.LEGACY_VPN : cfg.user;
+            mVpnManager.prepareVpn(user, VpnConfig.LEGACY_VPN, mVpnUserId);
         }
     }
 
@@ -396,7 +470,8 @@ public class SecurityControllerImpl extends CurrentUserTracker implements Securi
 
     private String getNameForVpnConfig(VpnConfig cfg, UserHandle user) {
         if (cfg.legacy) {
-            return mContext.getString(R.string.legacy_vpn_name);
+            return cfg.session != null
+                    ? cfg.session : mContext.getString(R.string.legacy_vpn_name);
         }
         // The package name for an active VPN is stored in the 'user' field of its VpnConfig
         final String vpnPackage = cfg.user;